24955c7f035b5c98794821ec05ab3dc7dd6610dd
ShamimaNasrin <shamima.nasrion007@gmail.com>
2022-11-25 02:16:08 +0600

    advertise

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -4,9 +4,9 @@ import './Banner.css';
 
 const Banner = () => {
     return (
-        <div className='rounded-[30px]'>
-            <div className="hero md:h-[65vh] lg:min-h-[85vh] rounded-[30px] banner-bg">
-                <div className="hero-overlay bg-opacity-60 rounded-[30px]"></div>
+        <div className=''>
+            <div className="hero md:h-[65vh] lg:min-h-[85vh] banner-bg">
+                <div className="hero-overlay bg-opacity-60"></div>
                 <div className="hero-content text-center text-white">
                     <div className="max-w-md">
                         <h1 className="mb-5 text-5xl font-bold ">Buy and sell your books
--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import Advertise from '../Advertise/Advertise';
 import Banner from '../Banner/Banner';
 import Contact from '../Contact/Contact';
 
 const Home = () => {
     return (
-        <div className='max-w-[1450px] mx-auto'>
+        <div className=''>
             <Banner></Banner>
+            <Advertise></Advertise>
             <Contact></Contact>
         </div>
     );
