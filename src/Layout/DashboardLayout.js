8337f0e71e52a04383dd4dbb0a5ceb90de69aae9
ShamimaNasrin <shamima.nasrion007@gmail.com>
2022-11-26 02:02:51 +0600

    create admin hooks and options

--- src/Layout/DashboardLayout.js
+++ src/Layout/DashboardLayout.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link, Outlet } from 'react-router-dom';
+import { AuthContext } from '../contexts/AuthProvider/AuthProvider';
+import useAdmin from '../Hooks/useAdmin';
 import Footer from '../Pages/Shared/Footer/Footer';
 import Navbar from '../Pages/Shared/Navbar/Navbar';
 
 const DashboardLayout = () => {
+
+    const { user } = useContext(AuthContext);
+    const [isAdmin] = useAdmin(user?.email);
+
     return (
         <div>
             <Navbar></Navbar>
@@ -15,8 +21,15 @@ const DashboardLayout = () => {
                 <div className="drawer-side bg-secondary">
                     <label htmlFor="dashboard-drawer" className="drawer-overlay"></label>
                     <ul className="menu p-4 w-80 text-base-content">
-                        <li><Link to="/dashboard" className='border border-yellow-600 rounded-md mb-2'>My Orders</Link></li>
+                        <li><Link to="/dashboard" className='border border-primary rounded-md mb-2'>My Orders</Link></li>
 
+                        {
+                            isAdmin && <>
+                                <li><Link to="/dashboard/allusers">All Sellers</Link></li>
+                                <li><Link to="/dashboard/adddoctor">All Buyers</Link></li>
+                                <li><Link to="/dashboard/managedoctors">Reported Items</Link></li>
+                            </>
+                        }
 
                     </ul>
 
