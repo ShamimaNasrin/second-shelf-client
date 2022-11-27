c43ddb914be4e6d3b0413883355174341d53534f
ShamimaNasrin <shamima.nasrion007@gmail.com>
2022-11-27 14:56:52 +0600

    delete a seller

--- src/Pages/Dashboard/AdminPages/AllSellers.js
+++ src/Pages/Dashboard/AdminPages/AllSellers.js
@@ -1,11 +1,14 @@
 import { useQuery } from '@tanstack/react-query';
-import React from 'react';
+import React, { useState } from 'react';
 import toast from 'react-hot-toast';
+import Loading from '../../Shared/Loading/Loading';
+import DeleteConfirmModal from '../DeleteConfirmModal/DeleteConfirmModal';
 
 const AllSellers = () => {
+    const [deleteUser, setDeleteUser] = useState(null);
 
     //load all sellers
-    const { data: sellers = [], refetch } = useQuery({
+    const { data: sellers = [], isLoading, refetch } = useQuery({
         queryKey: ['sellers'],
         queryFn: async () => {
             const res = await fetch('http://localhost:5000/sellers');
@@ -25,12 +28,39 @@ const AllSellers = () => {
         })
             .then(res => res.json())
             .then(data => {
-                console.log(data);
+                //console.log(data);
                 if (data.modifiedCount > 0) {
                     toast.success('Seller verified')
                     refetch();
                 }
             })
+    }
+
+    //modal functions
+    const closeModal = () => {
+        setDeleteUser(null);
+    }
+
+    //delete seller
+    const handleUserDelete = seller => {
+        fetch(`http://localhost:5000/users/${seller._id}`, {
+            method: 'DELETE',
+            headers: {
+                authorization: `bearer ${localStorage.getItem('accessToken')}`
+            }
+        })
+            .then(res => res.json())
+            .then(data => {
+                //console.log(data);
+                if (data.deletedCount > 0) {
+                    refetch();
+                    toast.success(`seller deleted successfully`)
+                }
+            })
+    }
+
+    if (isLoading) {
+        return <Loading></Loading>
     }
 
     return (
@@ -54,13 +84,25 @@ const AllSellers = () => {
                                 <td>{seller.name}</td>
                                 <td>{seller.email}</td>
                                 <td>{seller?.sellerType !== 'verified' && <button onClick={() => handleVerifiedSeller(seller._id)} className='btn btn-xs btn-primary'>Verify Seller</button>}</td>
-                                <td><button className='btn btn-xs btn-danger text-white p-4 text-center'>Delete</button></td>
+                                <td><label onClick={() => setDeleteUser(seller)} htmlFor="confirmation-modal" className="btn btn-sm btn-error text-white">Delete</label></td>
                             </tr>)
                         }
 
                     </tbody>
                 </table>
             </div>
+
+            {
+                deleteUser && <DeleteConfirmModal
+                    title={`Delete this seller?`}
+                    message={`If you delete ${deleteUser.name}. It cannot be restore`}
+                    successButtonName="Delete"
+                    successAction={handleUserDelete}
+                    modalData={deleteUser}
+                    closeModal={closeModal}
+                >
+                </DeleteConfirmModal>
+            }
         </div>
     );
 };
