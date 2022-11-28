b4645612419fdf9264214e04269785cab34859d2
ShamimaNasrin <shamima.nasrion007@gmail.com>
2022-11-28 14:51:21 +0600

    load and show advertised books

--- src/Pages/Home/Advertise/Advertise.js
+++ src/Pages/Home/Advertise/Advertise.js
@@ -1,19 +1,43 @@
+import { useQuery } from '@tanstack/react-query';
 import React from 'react';
+import Loading from '../../Shared/Loading/Loading';
 import AdvertiseCard from './AdvertiseCard';
 
 const Advertise = () => {
-    
+
+    //load all buyers
+    const { data: advertisedbooks = [], isLoading } = useQuery({
+        queryKey: ['advertisedbooks'],
+        queryFn: async () => {
+            const res = await fetch('http://localhost:5000/books/advertised');
+            const data = await res.json();
+            return data;
+        }
+    });
+
+    //console.log(advertisedbooks);
+
+    if (isLoading) {
+        return <Loading></Loading>
+    }
+
     return (
-        <section className='lg:mt-24 md:mt-16 sm:mt-9 mt-5 lg:px-10 md:px-7 px-3'>
-            <h2 className='font-bold text-primary text-center'>Advertisement</h2>
-            <h2 className='text-3xl text-center mb-10'>Available Books</h2>
-            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 mx-auto px-7 lg:mt-0 md:mt-0 mt-10 items-center justify-items-center justify-center' >
+        <section>
             {
-                [...Array(6)].map((card, i) =><AdvertiseCard
-                key={i}
-                card={card}></AdvertiseCard>)
+                advertisedbooks && <div className='lg:mt-24 md:mt-16 sm:mt-9 mt-5 lg:px-10 md:px-7 px-3'>
+                    <h2 className='font-bold text-primary text-center'>Advertisement</h2>
+                    <h2 className='text-3xl text-center mb-10'>Available Books</h2>
+                    <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 mx-auto px-7 lg:mt-0 md:mt-0 mt-10 items-center justify-items-center justify-center' >
+                        {
+                            advertisedbooks.map((book) => <AdvertiseCard
+                                key={book._id}
+                                book={book}>
+                            </AdvertiseCard>)
+                        }
+                    </div>
+                </div>
             }
-        </div>
+
         </section>
     );
 };
--- src/Pages/Home/Advertise/AdvertiseCard.js
+++ src/Pages/Home/Advertise/AdvertiseCard.js
@@ -1,19 +1,21 @@
 import React from 'react';
-import book from '../../../images/Books/hobbit.jpg';
 import { RiStarFill } from "react-icons/ri";
 
-const AdvertiseCard = () => {
+const AdvertiseCard = (book) => {
+    //console.log(book);
+    const { image, bookName, author, resalePrice, category } = book.book;
+    //console.log(bookName,author, resalePrice,category);
+
     return (
         <div>
             <div className="card lg:card-side bg-base-100 shadow-xl lg:max-w-max lg:max-h-[240px] md:max-h-[360px] md:max-w-[330px] sm:max-h-[360px] sm:max-w-[330px]">
-                <figure className='lg:w-2/5'><img src={book} alt="Album" /></figure>
+                <figure className='lg:w-2/5'><img src={image} alt="Album" /></figure>
                 <div className="card-body">
-                    <h2 className="card-title">The Hobbit</h2>
-                    <p>By J.R.R Tolkien</p>
+                    <h2 className="card-title">{bookName}</h2>
+                    <p>{author}</p>
+                    <p>{resalePrice}</p>
                     <p className='flex items-center'><RiStarFill className='text-yellow-300 mr-1' />4.5</p>
-                    <div className="card-actions justify-end">
-                        <button className="btn btn-primary">Listen</button>
-                    </div>
+                    <p className='text-red'>To buy this book check in the ${category} category</p>
                 </div>
             </div>
         </div>
